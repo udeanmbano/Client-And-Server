8bb2b166c1852b4cefe619bcc2d0091c1760e727
Udean Mbano <udeanmbano123@hotmail.co.uk>
2015-03-18 10:01:15 +0200

    Client Server
    Java Networking communication between client and server

---
+++ Server/src/mgi/Client.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mgi;
+
+/**
+ *
+ * @author Mbano
+ */
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class Client extends JFrame {
+// Text field for receiving radius
+private JTextField jtf = new JTextField();
+// Text area to display contents
+private JTextArea jta = new JTextArea();
+// IO streams
+private DataOutputStream toServer;
+private DataInputStream fromServer;
+
+public static void main(String[] args) {
+new Client();
+}
+public Client() {
+// Panel p to hold the label and text field
+JPanel p = new JPanel();
+p.setLayout(new BorderLayout());
+p.add(new JLabel("Enter radius"), BorderLayout.WEST);
+p.add(jtf, BorderLayout.CENTER);
+jtf.setHorizontalAlignment(JTextField.RIGHT);
+setLayout(new BorderLayout());
+add(p, BorderLayout.NORTH);
+add(new JScrollPane(jta), BorderLayout.CENTER);
+jtf.addActionListener(new TextFieldListener());
+
+setTitle("Client");
+setSize(500, 300);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setVisible(true); // It is necessary to show the frame here!
+
+try {
+
+    // IPV$ and socket number
+    Socket socket = new Socket("localhost", 3434);
+    
+    
+fromServer = new DataInputStream(socket.getInputStream());
+    
+
+toServer = new DataOutputStream(socket.getOutputStream());
+
+}
+
+catch (IOException ex) {
+jta.append(ex.toString() + '\n');
+     }
+
+}
+private class TextFieldListener implements ActionListener {
+@Override
+public void actionPerformed(ActionEvent e) {
+try {
+// Get the radius from the text field
+double radius = Double.parseDouble(jtf.getText().trim());
+
+
+toServer.writeDouble(radius);
+toServer.flush();
+
+double area = fromServer.readDouble();
+
+jta.append("Radius is " + radius + "\n");
+jta.append("Area received from the server is "
++ area + '\n');
+
+}
+
+catch (IOException ex) {
+System.err.println(ex);
+}
+
+}
+}
+
+}
---
+++ Server/src/mgi/Server.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mgi;
+
+/**
+ *
+ * @author Mbano
+ */
+ import java.io.*;
+ import java.net.*;
+ import java.util.*;
+ import java.awt.*;
+ import javax.swing.*;
+import mgi.Client;
+public class Server extends JFrame {
+ // Text area for displaying contents
+ private JTextArea jta = new JTextArea();
+
+ public static void main(String[] args) {
+ new Server();
+ }
+ public Server() {
+ // Place text area on the frame
+ setLayout(new BorderLayout());
+ add(new JScrollPane(jta), BorderLayout.CENTER);
+ setTitle("Server");
+ setSize(500, 300);
+ setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ setVisible(true); // It is necessary to show the frame here!
+
+ try {
+ // Create a server socket
+ServerSocket serverSocket = new ServerSocket(8000);
+
+ jta.append("Server started at " + new Date() + '\n' );
+
+ // Listen for a connection request
+ // Listen for a connection request
+
+Socket socket = serverSocket.accept();
+ // Create data input and output streams
+DataInputStream inputFromClient = new DataInputStream(
+socket.getInputStream());
+DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+
+ while (true) {
+ // Receive radius from the client
+double radius = inputFromClient.readDouble();
+
+ // Compute area
+ double area = radius * radius * Math.PI;
+
+ // Send area back to the client
+
+ jta.append("Radius received from client: " + radius + '\n' );
+ jta.append("Area found: " + area + '\n' ); }
+ }
+ catch(IOException ex) {
+ System.err.println(ex);
+ }
+ }
+}
+ 
+  
